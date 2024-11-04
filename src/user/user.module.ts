f1a7c572f7f4b213a0b98eb519dfe9b22698bfc0
fauzi niko <fauziniko12@gmail.com>
2024-11-04 23:49:26 +0700

    conection

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,7 +1,35 @@
 import { Module } from '@nestjs/common';
 import { UserController } from './user/user.controller';
+import { UserService } from './user/user.service';
+import { Connection, createConnection } from './connection/connection';
+import { mailService, MailService } from './mail/mail.service';
+import { UserRepository } from './user-repository/user-repository';
+import { MemberService } from './member/member.service';
+import { MemberService } from './member/member.service';
+import { ConfigService } from '@nestjs/config';
+import { UserRepository } from './user-repository/user-repository';
 
 @Module({
-  controllers: [UserController]
+  imports: [],
+  controllers: [UserController],
+  providers: [
+    UserService,
+    {
+      provide: Connection,
+      useFactory: createConnection,
+      inject: [ConfigService],
+    },
+    {
+      provide: MailService,
+      useValue: mailService,
+    },
+    {
+      provide: 'EmailService',
+      useExisting: MailService,
+    },
+    UserRepository,
+    MemberService,
+  ],
+  exports: [UserService],
 })
 export class UserModule {}
--- src/user/user/user.controller.ts
+++ src/user/user/user.controller.ts
@@ -1,8 +1,104 @@
-import { Controller, Get, Header, HttpCode, HttpRedirectResponse, Param, Query, Redirect, Res } from "@nestjs/common";
-import { Response } from 'express';
+import {
+  Body,
+  Controller,
+  Get,
+  Header,
+  HttpCode,
+  HttpException,
+  HttpRedirectResponse,
+  Inject,
+  Param,
+  ParseIntPipe,
+  Post,
+  Query,
+  Redirect,
+  Req,
+  Res,
+  UseFilters,
+  UseInterceptors,
+  UsePipes,
+} from '@nestjs/common';
+import { Request, Response } from 'express';
+import { UserService } from './user.service';
+import { Connection } from '../connection/connection';
+import { MailService } from '../mail/mail.service';
+import { UserRepository } from '../user-repository/user-repository';
+import { MemberService } from '../member/member.service';
+import { User } from '@prisma/client';
+import { ValidationFilter } from '../../validation/validation.filter';
+import {
+  LoginUserRequest,
+  loginUserRequestValidation,
+} from '../../model/login.model';
+import { ValidationPipe } from '../../validation/validation.pipe';
+import { TimeInterceptor } from '../../time/time.interceptor';
+import { Auth } from '../../auth/auth.decorator';
+import { Roles } from '../../role/roles.decorator';
 
 @Controller('/api/users')
 export class UserController {
+  constructor(
+    private service: UserService,
+    private connection: Connection,
+    private mailService: MailService,
+    @Inject('EmailService') private emailService: MailService,
+    private userRepository: UserRepository,
+    private memberService: MemberService,
+  ) {}
+
+  @Get('/current')
+  @Roles(['admin', 'operator'])
+  current(@Auth() user: User): Record<string, any> {
+    return {
+      data: `Hello ${user.first_name} ${user.last_name}`,
+    };
+  }
+
+  @UsePipes(new ValidationPipe(loginUserRequestValidation))
+  @UseFilters(ValidationFilter)
+  @Post('/login')
+  @Header('Content-Type', 'application/json')
+  @UseInterceptors(TimeInterceptor)
+  login(@Query('name') name: string, @Body() request: LoginUserRequest) {
+    return {
+      data: `Hello ${request.username}`,
+    };
+  }
+
+  @Get('/connection')
+  async getConnection(): Promise<string> {
+    this.mailService.send();
+    this.emailService.send();
+
+    console.info(this.memberService.getConnectionName());
+    this.memberService.sendEmail();
+
+    return this.connection.getName();
+  }
+
+  @Get('/create')
+  async create(
+    @Query('first_name') firstName: string,
+    @Query('last_name') lastName: string,
+  ): Promise<User> {
+    if (!firstName) {
+      throw new HttpException(
+        {
+          code: 400,
+          errors: 'first_name is required',
+        },
+        400,
+      );
+    }
+    return this.userRepository.save(firstName, lastName);
+  }
+
+  @Get('/hello')
+  // @UseFilters(ValidationFilter)
+  async sayHello(@Query('name') name: string): Promise<string> {
+    return this.service.sayHello(name);
+  }
+
   @Get('/view/hello')
   viewHello(@Query('name') name: string, @Res() response: Response) {
     response.render('index.html', {
@@ -11,34 +107,48 @@ export class UserController {
     });
   }
 
+  @Get('/set-cookie')
+  setCookie(@Query('name') name: string, @Res() response: Response) {
+    response.cookie('name', name);
+    response.status(200).send('Success Set Cookie');
+  }
+
+  @Get('/get-cookie')
+  getCookie(@Req() requet: Request): string {
+    return requet.cookies['name'];
+  }
 
   @Get('/sample-response')
   @Header('Content-Type', 'application/json')
   @HttpCode(200)
   sampleResponse(): Record<string, string> {
     return {
-      "data": "Hello World!"
-    }
- }
+      data: 'Hello JSON',
+    };
+  }
 
- @Get('/redirect')
- @Redirect()
- redirect(): HttpRedirectResponse {
+  @Get('/redirect')
+  @Redirect()
+  redirect(): HttpRedirectResponse {
     return {
-      url: 'api/users/sample-response',
+      url: '/api/users/sample-response',
       statusCode: 301,
-    }
- }
-  @Get('/hello')
-  sayHello(
-    @Query('first_name') firstName: string,
-    @Query('first_email') lastName: string,
-  ): string {
-    return `hello ${firstName} ${lastName}`;
+    };
   }
 
   @Get('/:id')
-  get(@Param('id') id: string): string {
+  getById(@Param('id', ParseIntPipe) id: number): string {
+    console.info(id * 10);
     return `GET ${id}`;
+  }
+
+  @Post()
+  post(): string {
+    return 'POST';
+  }
+
+  @Get('/sample')
+  get(): string {
+    return 'Hello NestJS';
   }
 }
