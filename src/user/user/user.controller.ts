e301bd6f693cdb0ae71717d2c46ae9972821b226
fauzi niko <fauziniko12@gmail.com>
2024-11-04 11:25:11 +0700

    user controller2

--- src/user/user/user.controller.ts
+++ src/user/user/user.controller.ts
@@ -1,13 +1,33 @@
-import { Controller, Get, Param, Query } from '@nestjs/common';
+import { Controller, Get, Header, HttpCode, HttpRedirectResponse, Param, Query, Redirect, Res } from "@nestjs/common";
+import { Response } from 'express';
+
 @Controller('/api/users')
 export class UserController {
+  @Get('/sample-response')
+  @Header('Content-Type', 'application/json')
+  @HttpCode(200)
+  sampleResponse(): Record<string, string> {
+    return {
+      "data": "Hello World!"
+    }
+ }
+
+ @Get('/redirect')
+ @Redirect()
+ redirect(): HttpRedirectResponse {
+    return {
+      url: 'api/users/sample-response',
+      statusCode: 301,
+    }
+ }
   @Get('/hello')
   sayHello(
-    @Query('frist_name') firstName: string,
-    @Query('frist_email') lastName: string,
+    @Query('first_name') firstName: string,
+    @Query('first_email') lastName: string,
   ): string {
     return `hello ${firstName} ${lastName}`;
   }
+
   @Get('/:id')
   get(@Param('id') id: string): string {
     return `GET ${id}`;
