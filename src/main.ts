b83169ba96a030b122666384ed2f2a658182a5c1
fauzi niko <fauziniko12@gmail.com>
2024-11-04 22:15:55 +0700

    test

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as cookieParser from 'cookie-parser';
+import {NestExpressApplication} from "@nestjs/platform-express";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(cookieParser('rahasia'));
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
--- src/user/user/user.controller.ts
+++ src/user/user/user.controller.ts
@@ -3,6 +3,15 @@ import { Response } from 'express';
 
 @Controller('/api/users')
 export class UserController {
+  @Get('/view/hello')
+  viewHello(@Query('name') name: string, @Res() response: Response) {
+    response.render('index.html', {
+      title: 'Template Engine',
+      name: name,
+    });
+  }
+
+
   @Get('/sample-response')
   @Header('Content-Type', 'application/json')
   @HttpCode(200)
