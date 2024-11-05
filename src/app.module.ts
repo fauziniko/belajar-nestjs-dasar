ed7f299831e1f45ed2501a7eda65ac80ea2587c1
fauzi niko <fauziniko12@gmail.com>
2024-11-05 23:07:56 +0700

    validation module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,14 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { ConfigModule } from "@nestjs/config";
+import { ValidationModule } from './validation/validation.module';
 
 @Module({
   imports: [ConfigModule.forRoot({
     isGlobal: true,
   }),
-    UserModule
+    UserModule,
+    ValidationModule
   ],
   controllers: [AppController],
   providers: [AppService],
