30b99bbaf4ae0a404c56279bfbc6a38919cef7d5
fauzi niko <fauziniko12@gmail.com>
2024-11-05 23:21:26 +0700

    middleware2

--- src/log/log.middleware.ts
+++ src/log/log.middleware.ts
@@ -1,8 +1,13 @@
-import { Injectable, NestMiddleware } from '@nestjs/common';
+import { Inject, Injectable, NestMiddleware } from '@nestjs/common';
+import { Request, Response } from 'express';
+import { WINSTON_MODULE_PROVIDER } from 'nest-winston';
+import { Logger } from 'winston';
 
 @Injectable()
-export class LogMiddleware implements NestMiddleware {
-  use(req: any, res: any, next: () => void) {
+export class LogMiddleware implements NestMiddleware<Request, Response> {
+  constructor(@Inject(WINSTON_MODULE_PROVIDER) private logger: Logger) {}
+  use(req: Request, res: Response, next: () => void) {
+    this.logger.info(`Receive request for URL : ${req.url}`);
     next();
   }
 }
